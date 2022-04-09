982e8e6944bafea8216f33bb8318af23173a5796
Samuel Bacellar <samuelbacellar1@gmail.com>
2022-04-09 16:36:34 -0300

    Criando código do projeto
    Projeto programado em C# (orientação a objetos) que imprime uma mensagem na tela.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooMensagem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mensagem msg1, msg2;
+            msg1 = new Mensagem();
+            msg1.TextoMensagem = "Alô Mundo";
+            msg1.ExibirMensagem();
+
+            //msg2 = new Mensagem();
+            //msg2.TextoMensagem = "Segundo objeto";
+            //msg2.ExibirMensagem();
+            Console.ReadKey();
+        }
+    }
+}
